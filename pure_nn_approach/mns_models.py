1c213ccf713b658aa8276fade3859dff95c15546
YukaMachino <80087816+yukam997@users.noreply.github.com>
2023-08-19 16:29:01 -0300

    Update mns_models.py

--- pure_nn_approach/mns_models.py
+++ pure_nn_approach/mns_models.py
@@ -40,6 +40,27 @@ class three_channels_Net(nn.Module):
         x = self.fc3(x)
         return x
 
+class four_gathered_Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 11 * 11, 480)
+        self.fc2 = nn.Linear(480, 120)
+        self.fc3 = nn.Linear(120, 30)
+        self.fc4 = nn.Linear(30, 2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+
 class five_channels_Net(nn.Module):
     def __init__(self):
         super().__init__()
